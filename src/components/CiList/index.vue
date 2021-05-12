3101dc6b58a3394c0c19f7e600fb59ba6d21aa6b
longzihai <1306829830@qq.com>
2021-05-13 00:15:17 +0800

    add createComponents

---
+++ src/components/CiList/index.vue
@@ -0,0 +1,40 @@
+<template>
+			<div class="cinema_body">
+					<ul>
+						<li>
+							<div>
+								<span></span>
+								<span class="q"><span class="price"></span> 元起</span>
+							</div>
+							<div class="address">
+								<span></span>
+								<span></span>
+							</div>
+							<div class="card">
+								<div></div>
+							</div>
+						</li>
+					</ul>
+			</div>
+</template>
+
+<script>
+export default{
+    name:'CiList',
+}
+</script>
+
+<style scoped>
+#content .cinema_body{ flex:1; overflow:auto;}
+.cinema_body ul{ padding:20px;}
+.cinema_body li{  border-bottom:1px solid #e6e6e6; margin-bottom: 20px;}
+.cinema_body div{ margin-bottom: 10px;}
+.cinema_body .q{ font-size: 11px; color:#f03d37;}
+.cinema_body .price{ font-size: 18px;}
+.cinema_body .address{ font-size: 13px; color:#666;}
+.cinema_body .address span:nth-of-type(2){ float:right; }
+.cinema_body .card{ display: flex;}
+.cinema_body .card div{ padding: 0 3px; height: 15px; line-height: 15px; border-radius: 2px; color: #f90; border: 1px solid #f90; font-size: 13px; margin-right: 5px;}
+.cinema_body .card div.or{ color: #f90; border: 1px solid #f90;}
+.cinema_body .card div.bl{ color: #589daf; border: 1px solid #589daf;}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,21 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import movieRouter from './movie'
+import mineRouter from './mine'
+import cinemaRouter from './cinema'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default new Router({
+  mode:'history', 
+  base:process.env.BASE_URL,
+  routes:[
+    movieRouter,
+    mineRouter,
+    cinemaRouter,
+    {
+      path:'/*',
+      redirect:'/movie'
+    }
+  ]
 })
-
-export default router
